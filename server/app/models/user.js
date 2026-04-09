fb6328c7b8e3f455ff2257021cb47e37838c0906
“Bayehd” <Benoniaowusu@gmail.com>
2026-04-08 23:09:29 -0400

    updates

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -28,4 +28,25 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+    // Only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) return next();
+    
+    try {
+        // Generate salt and hash password
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Method to compare password for login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+
 module.exports = mongoose.model('User', userSchema);
